0cac427ac13289db397e3b2a3d108b0a3988cf3f
Alex <alexbachurin91@gmail.com>
2022-06-05 05:01:25 +0300

    error page completed

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,8 +1,21 @@
 import React from "react";
-import styled from "styled-components";
+import Wrapper from "../assets/wrappers/Error/Error";
+import { Link } from "react-router-dom";
+import img from "../assets/images/not-found.svg";
 
 const ErrorPage = () => {
-	return <div>ErrorPage</div>;
+	return (
+		<Wrapper className="full-page">
+			<div>
+				<img src={img} alt="not-found" />
+				<h3>Ohh! page not found</h3>
+				<p>There is nothing, but chickens &#9785;</p>
+				<Link className="btn" to={"/"}>
+					back home
+				</Link>
+			</div>
+		</Wrapper>
+	);
 };
 
 export default ErrorPage;
