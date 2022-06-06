cb1499d1eca07057b4b0e7fb4ee94383ef29162d
Alex <alexbachurin91@gmail.com>
2022-06-06 20:18:28 +0300

    register page setup

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,7 +1,49 @@
-import React from "react";
-
-const RegisterPage = () => {
-	return <div>RegisterPage</div>;
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Logo } from "../components";
+const initialState = {
+	name: "",
+	email: "",
+	password: "",
+	isMember: true,
 };
+const RegisterPage = () => {
+	const [values, setValues] = useState(initialState);
+
+	const handleChange = (e) => {
+		console.log(e.target);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(e.target);
+	};
+
+	return (
+		<Wrapper className="full-page">
+			<form className="form" onSubmit={handleSubmit}>
+				<Logo />
+				{/* name field */}
+				<div className="form-row">
+					<label htmlFor="name" className="form-label">
+						name
+					</label>
+					<input
+						type="text"
+						name="name"
+						value={values.name}
+						onChange={handleChange}
+						className="form-input"
+					/>
+				</div>
+				<button className="btn btn-block" type="submit">
+					Submit
+				</button>
+			</form>
+		</Wrapper>
+	);
+};
+
+const Wrapper = styled.div``;
 
 export default RegisterPage;
