c18d6a7882b2b2064b281b63a73f50e6524dd125
Alex <alexbachurin91@gmail.com>
2022-06-06 21:55:55 +0300

    login and register thunks/placeholders

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -6,6 +6,22 @@ const initialState = {
 	user: null,
 };
 
+//register thunk
+export const registerUser = createAsyncThunk(
+	"user/registerUser",
+	async (user, thunkApi) => {
+		console.log(`Register user : ${user}`);
+	}
+);
+
+//login thunk
+export const loginUser = createAsyncThunk(
+	"user/loginUser",
+	async (user, thunkApi) => {
+		console.log(`Login user : ${user}`);
+	}
+);
+
 //create slice
 const userSlice = createSlice({
 	name: "user",
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Login, Register } from "../components";
-import { Logo, FormRow } from "../components";
+import { Logo } from "../components";
 import Wrapper from "../assets/wrappers/Register/Register";
 import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser, registerUser } from "../features/user/userSlice";
 const initialState = {
 	name: "",
 	email: "",
@@ -11,7 +13,10 @@ const initialState = {
 };
 const RegisterPage = () => {
 	const [values, setValues] = useState(initialState);
-	const formRef = useRef(null);
+
+	const { user, isLoading } = useSelector((store) => store.user);
+	const dispatch = useDispatch();
+
 	const handleChange = (e) => {
 		//get targetted row name and set input value to state
 		const rowName = e.target.name;
@@ -23,14 +28,18 @@ const RegisterPage = () => {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		if (
-			(!values.name && !values.isMember) ||
-			!values.email ||
-			!values.password
-		) {
+		const { name, isMember, email, password } = values;
+		if ((!name && !isMember) || !email || !password) {
 			toast.error("Please Fill Out All Fields");
 			return;
 		}
+		//if user is member already dispatch login
+		if (isMember) {
+			dispatch(loginUser({ email, password }));
+			return;
+		}
+		//if not dispatch register
+		dispatch(registerUser({ name, email, password }));
 	};
 
 	//change member
@@ -40,7 +49,7 @@ const RegisterPage = () => {
 
 	return (
 		<Wrapper className="full-page">
-			<form ref={formRef} className="form" onSubmit={handleSubmit}>
+			<form className="form" onSubmit={handleSubmit}>
 				<Logo />
 				{values.isMember ? (
 					<Login
