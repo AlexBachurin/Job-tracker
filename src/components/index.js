7819b0ebdecb69c29ddbbbe6a4f93e19286bff5c
Alex <alexbachurin91@gmail.com>
2022-06-07 03:41:14 +0300

    complete SharedLayout setup

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,7 @@ import Logo from "./Logo";
 import FormRow from "./FormRow";
 import Login from "./Login";
 import Register from "./Register";
-export { Logo, FormRow, Login, Register };
+import BigSidebar from "./BigSidebar";
+import SmallSidebar from "./SmallSidebar";
+import Navbar from "./Navbar";
+export { Logo, FormRow, Login, Register, BigSidebar, SmallSidebar, Navbar };
