5bd490aabad846b1988fe6d1a5a0c5c370c65a00
Alex <alexbachurin91@gmail.com>
2022-06-07 05:10:40 +0300

    added NavLinks to separate component since they are reusable

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/SmallSidebar/SmallSidebar";
 import { FaTimes } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
+
 import { Logo } from "./";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleSidebar } from "../features/user/userSlice";
-import links from "../utils/links";
+import NavLinks from "./NavLinks";
 const SmallSidebar = () => {
 	const { isSidebarOpen } = useSelector((store) => store.user);
 	const dispatch = useDispatch();
@@ -24,25 +24,7 @@ const SmallSidebar = () => {
 					<header>
 						<Logo />
 					</header>
-					<div className="nav-links">
-						{links.map((link) => {
-							const { id, text, path, icon } = link;
-							// make link active by using isActive NavLnik functionality
-							return (
-								<NavLink
-									to={path}
-									className={({ isActive }) => {
-										return isActive ? "nav-link active" : "nav-link";
-									}}
-									id={id}
-									onClick={() => dispatch(toggleSidebar())}
-								>
-									<span className="icon">{icon}</span>
-									{text}
-								</NavLink>
-							);
-						})}
-					</div>
+					<NavLinks toggleSidebar={toggleSidebar} />
 				</div>
 			</div>
 		</Wrapper>
