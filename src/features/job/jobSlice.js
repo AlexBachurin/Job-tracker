dc4e13368c06ef4be1f72f39ac9d84fa36960407
Alex <alexbachurin91@gmail.com>
2022-06-16 12:28:49 +0300

    jobSlice setup

---
+++ src/features/job/jobSlice.js
@@ -0,0 +1,20 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { customUrl } from "../../utils/axios";
+import { getUserFromLocalStorage } from "../../utils/localStorage";
+
+const initialState = {
+	isLoading: false,
+	position: "",
+	company: "",
+	jobLocation: "",
+	jobTypeOptions: ["full-time", "part-time", "remote", "internship"],
+	defaultJobType: "full-time",
+	statusOptions: ["interview", "declined", "pending"],
+	defaultStatus: "pending",
+	isEditing: false,
+	editJobId: "",
+};
+
+const jobSlice = createSlice({});
+
+export default jobSlice.reducer;
