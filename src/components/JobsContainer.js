3c5070a6cf3596ea8f33e5333a9611e1a4951a28
Alex <alexbachurin91@gmail.com>
2022-06-16 17:37:49 +0300

    JobsContainer setup + Loading component added

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -1,7 +1,29 @@
 import React from "react";
-
+import { useEffect } from "react";
+import Job from "./Job";
+import Wrapper from "../assets/wrappers/JobsContainer/JobsContainerWrapper";
+import { useSelector, useDispatch } from "react-redux";
+import Loading from "./Loading";
 const JobsContainer = () => {
-	return <div>JobsContainer</div>;
+	const { jobs, isLoading } = useSelector((store) => store.allJobs);
+	const dispatch = useDispatch();
+
+	//if loading show spinner
+	if (isLoading) {
+		return <Loading center={true} />;
+	}
+	//if no jobs found display msg
+	if (jobs.length === 0) {
+		return <Wrapper>No Jobs to display</Wrapper>;
+	}
+	return (
+		<Wrapper>
+			{jobs.map((item) => {
+				console.log(item);
+				return <Job key={item._id} {...item} />;
+			})}
+		</Wrapper>
+	);
 };
 
 export default JobsContainer;
