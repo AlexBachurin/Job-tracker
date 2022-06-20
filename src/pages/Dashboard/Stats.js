b1cbd1432d2e2a6bf921c4877d7c3e730dd57d29
Alex <alexbachurin91@gmail.com>
2022-06-20 20:01:33 +0300

    stats page structure

--- src/pages/Dashboard/Stats.js
+++ src/pages/Dashboard/Stats.js
@@ -1,15 +1,30 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import ChartContainer from "../../components/ChartContainer";
+import Loading from "../../components/Loading";
+import StatsContainer from "../../components/StatsContainer";
 import { getStats } from "../../features/allJobs/allJobsSlice";
 
 const Stats = () => {
+	const { isLoading, monthlyApplications } = useSelector(
+		(store) => store.allJobs
+	);
 	const dispatch = useDispatch();
 	//invoke stats on page load
 	useEffect(() => {
 		dispatch(getStats());
 		//eslint-disable-next-line
 	}, []);
-	return <div>Stats</div>;
+	if (isLoading) {
+		return <Loading center={true} />;
+	}
+	return (
+		<>
+			<StatsContainer />
+			{/* only display chart if we have something in array */}
+			{monthlyApplications.length > 0 ? <ChartContainer /> : null}
+		</>
+	);
 };
 
 export default Stats;
