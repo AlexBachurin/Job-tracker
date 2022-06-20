1af409c47f97a8f9e27f6d864a518e28b6df6389
Alex <alexbachurin91@gmail.com>
2022-06-20 20:12:15 +0300

    StatsContainer structure completed

--- src/components/StatsContainer.js
+++ src/components/StatsContainer.js
@@ -1,7 +1,40 @@
 import React from "react";
-
+import { FaBug, FaCalendarCheck, FaSuitcaseRolling } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import Wrapper from "../assets/wrappers/StatsContainer/StatsContainerWrapper";
+import StatsItem from "./StatsItem";
 const StatsContainer = () => {
-	return <div>StatsContainer</div>;
+	const { stats } = useSelector((store) => store.allJobs);
+	const defaultStats = [
+		{
+			title: "pending applications",
+			count: stats.pending || 0,
+			icon: <FaSuitcaseRolling />,
+			color: "#e9b949",
+			bcg: "#fcefc7",
+		},
+		{
+			title: "interviews scheduled",
+			count: stats.interview || 0,
+			icon: <FaCalendarCheck />,
+			color: "#647acb",
+			bcg: "#e0e8f9",
+		},
+		{
+			title: "declined applications",
+			count: stats.declined || 0,
+			icon: <FaBug />,
+			color: "#d66a6a",
+			bcg: "#ffeeee",
+		},
+	];
+	return (
+		<Wrapper>
+			{defaultStats.map((item, index) => {
+				return <StatsItem key={index} {...item} />;
+			})}
+		</Wrapper>
+	);
 };
 
 export default StatsContainer;
---
+++ src/components/StatsItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Wrapper from "../assets/wrappers/StatsItem/StatsItemWrapper";
+const StatsItem = ({ title, count, icon, color, bcg }) => {
+	return <Wrapper>StatsItem</Wrapper>;
+};
+
+export default StatsItem;
