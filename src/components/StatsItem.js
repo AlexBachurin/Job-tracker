7b6820a5172cb4ef1b0b859a0604be8b5c66f3d8
Alex <alexbachurin91@gmail.com>
2022-06-21 17:01:03 +0300

    StatsItem component added

--- src/components/StatsItem.js
+++ src/components/StatsItem.js
@@ -1,7 +1,15 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/StatsItem/StatsItemWrapper";
 const StatsItem = ({ title, count, icon, color, bcg }) => {
-	return <Wrapper>StatsItem</Wrapper>;
+	return (
+		<Wrapper color={color} bcg={bcg}>
+			<header>
+				<span className="count">{count}</span>
+				<span className="icon">{icon}</span>
+			</header>
+			<h5 className="title">{title}</h5>
+		</Wrapper>
+	);
 };
 
 export default StatsItem;
